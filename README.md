7f27bc9d16eb50b05358574f2d93d750fca247b2
kakoi-to-pirat <kakoi-to.pirat@yandex.ru>
2020-11-29 18:15:44 +0300

    add effector

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Simple mobx example
+Simple effector example
--- src/index.js
+++ src/index.js
@@ -1,47 +1,50 @@
-import { autorun, observable } from "mobx";
+import { createStore, createEvent, createEffect } from "effector";
 
-const state = observable({
+const LOADING_MESSAGE = "Loading";
+const LOADED_MESSAGE = "Loaded";
+
+const initalState = {
   count: 0,
-  status: {
-    isLoading: false,
-    message: "",
-  },
-  increment() {
-    this.count++;
-  },
-  decrement() {
-    this.count--;
-  },
-  toLoading() {
-    this.status.isLoading = true;
-    this.status.message = "Loading";
-  },
-  toLoaded() {
-    this.status.isLoading = false;
-    this.status.message = "Loaded";
-  },
-  asyncIncrement() {
-    this.toLoading();
-    setTimeout(() => {
-      this.increment();
-      this.toLoaded();
-    }, 1500);
-  },
-});
+  status: LOADED_MESSAGE,
+};
+
+const increment = createEvent();
+const decrement = createEvent();
+const asyncIncrement = createEffect(
+  (count) =>
+    new Promise((resolve, reject) => setTimeout(() => resolve(count), 1500))
+);
+
+const state = createStore(initalState)
+  .on(increment, (oldState, count) => ({
+    ...oldState,
+    count: oldState.count + count,
+  }))
+  .on(asyncIncrement.finally, (oldState, { result }) => ({
+    ...oldState,
+    count: oldState.count + result,
+    status: { isLoading: false, message: LOADED_MESSAGE },
+  }))
+  .on(asyncIncrement, (oldState) => ({
+    ...oldState,
+    status: { isLoading: true, message: LOADING_MESSAGE },
+  }))
+  .on(decrement, (oldState, count) => ({
+    ...oldState,
+    count: oldState.count - count,
+  }));
 
 const getElement = (id) => document.getElementById(id);
 
-const render = () => {
+const render = (state) => {
   getElement("status").textContent = state.status.message;
   getElement("counterInput").textContent = state.count.toString();
 };
 
-getElement("incBtn").addEventListener("click", () => state.increment());
+getElement("incBtn").addEventListener("click", () => increment(1));
 
-getElement("decBtn").addEventListener("click", () => state.decrement());
+getElement("decBtn").addEventListener("click", () => decrement(1));
 
-getElement("asyncIncBtn").addEventListener("click", () =>
-  state.asyncIncrement()
-);
+getElement("asyncIncBtn").addEventListener("click", () => asyncIncrement(1));
 
-autorun(render);
+state.watch(render);
